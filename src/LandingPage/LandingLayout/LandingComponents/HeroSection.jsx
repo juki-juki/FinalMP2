97b3db3039fa3dbfff50f4af382b3ab769e66cd2
Alvin Joshua Caponpon <alvincaponpon.ajc@gmail.com>
2023-11-04 04:00:08 +0800

    email working

--- src/LandingPage/LandingLayout/LandingComponents/HeroSection.jsx
+++ src/LandingPage/LandingLayout/LandingComponents/HeroSection.jsx
@@ -1,6 +1,27 @@
+import emailjs from 'emailjs-com';
 import React from 'react';
 
-const HeroSection = () => {
+
+function HeroSection () {
+    const sendEmail = (e) => {
+      e.preventDefault();
+      alert("submitted");
+      emailjs
+        .sendForm(
+          'service_m9a6ei9',
+          'template_gyh9awa',
+          e.target,
+          '4zdqAKlPVIH7M6Ztv' 
+        )
+        .then(
+          (result) => {
+            console.log('Email sent successfully', result.text);
+          },
+          (error) => {
+            console.error('Error sending email:', error.text);
+          }
+        );
+    };
   return (
     <section className="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
@@ -13,8 +34,8 @@ const HeroSection = () => {
         </a>
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Stay in the Game! </h1>
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">Subscribe to our newsletter and be the first to know about the latest game releases, exclusive offers, and exciting updates. Join our gaming community and level up your gaming experience. </p>
-        <form className="w-full max-w-md mx-auto">
-          <label htmlFor="default-email" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Email sign-up</label>
+        <form className="w-full max-w-md mx-auto" onSubmit={sendEmail}>
+          <label htmlFor="defaultEmail"  className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Email sign-up</label>
           <div className="relative">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
               <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
@@ -23,6 +44,7 @@ const HeroSection = () => {
               </svg>
             </div>
             <input
+              name="email_from"
               type="email"
               id="default-email"
               className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -30,6 +52,7 @@ const HeroSection = () => {
               required
             />
             <button
+              
               type="submit"
               className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
