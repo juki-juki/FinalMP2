27b77f951509be8a9f88aad9449086104f382646
Alvin Joshua Caponpon <alvincaponpon.ajc@gmail.com>
2023-11-05 02:07:33 +0800

    up

--- src/LandingPage/LandingLayout/LandingComponents/FacebookMessengerChat.jsx
+++ src/LandingPage/LandingLayout/LandingComponents/FacebookMessengerChat.jsx
@@ -4,7 +4,7 @@ import { CustomChat, FacebookProvider } from 'react-facebook';
 const FacebookMessengerChat = () => {
   return (
     <FacebookProvider appId="886578262937544" chatSupport>
-        <CustomChat pageId="140534082484292" minimized={true}/>
+        <CustomChat pageId="140534082484292" minimized="true"/>
       </FacebookProvider> 
   )
 }
--- src/LandingPage/LandingLayout/LandingComponents/NavigationBar.jsx.jsx
+++ src/LandingPage/LandingLayout/LandingComponents/NavigationBar.jsx.jsx
@@ -34,9 +34,9 @@ const NavigationBar = () => {
     offset={-70}
     duration={500}
   >
-    <a href="#" className="block py-2 pl-3 pr-4 text-white rounded hover-bg-gray-100 md:hover-bg-transparent md:hover-text-blue-700 md:p-0 md:dark-hover-text-blue-500 dark-text-white dark-hover-bg-gray-700 dark-hover-text-white md:dark-hover-bg-transparent dark-border-gray-700">
+    <div href="#" className="block py-2 pl-3 pr-4 text-white rounded hover-bg-gray-100 md:hover-bg-transparent md:hover-text-blue-700 md:p-0 md:dark-hover-text-blue-500 dark-text-white dark-hover-bg-gray-700 dark-hover-text-white md:dark-hover-bg-transparent dark-border-gray-700">
       Home
-    </a>
+      </div>
   </Link>
               <li>
   <Link
@@ -46,9 +46,9 @@ const NavigationBar = () => {
     offset={-70}
     duration={500}
   >
-    <a href="#" className="block py-2 pl-3 pr-4 text-white rounded hover-bg-gray-100 md:hover-bg-transparent md:hover-text-blue-700 md:p-0 md:dark-hover-text-blue-500 dark-text-white dark-hover-bg-gray-700 dark-hover-text-white md:dark-hover-bg-transparent dark-border-gray-700">
+    <div href="#" className="block py-2 pl-3 pr-4 text-white rounded hover-bg-gray-100 md:hover-bg-transparent md:hover-text-blue-700 md:p-0 md:dark-hover-text-blue-500 dark-text-white dark-hover-bg-gray-700 dark-hover-text-white md:dark-hover-bg-transparent dark-border-gray-700">
       About Us
-    </a>
+    </div>
   </Link>
 </li>
 <li>
@@ -59,9 +59,9 @@ const NavigationBar = () => {
     offset={-70}
     duration={500}
   >
-    <a href="#" className="block py-2 pl-3 pr-4 text-white rounded hover-bg-gray-100 md:hover-bg-transparent md:hover-text-blue-700 md:p-0 md:dark-hover-text-blue-500 dark-text-white dark-hover-bg-gray-700 dark-hover-text-white md:dark-hover-bg-transparent dark-border-gray-700">
+    <div href="#" className="block py-2 pl-3 pr-4 text-white rounded hover-bg-gray-100 md:hover-bg-transparent md:hover-text-blue-700 md:p-0 md:dark-hover-text-blue-500 dark-text-white dark-hover-bg-gray-700 dark-hover-text-white md:dark-hover-bg-transparent dark-border-gray-700">
       Services
-    </a>
+    </div>
   </Link>
 </li>
             </ul>
